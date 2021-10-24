9f9102fa5a63af46b1f232937288be1bb9178c84
Alex Gogl <alex@gogl.io>
2021-10-24 07:26:06 +0100

    fix add

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,39 +1,39 @@
 import {
   InboxAdded as InboxAddedEvent,
   KeyAdded as KeyAddedEvent,
-  Send as SendEvent
-} from "../generated/Messaging/Messaging"
-import { InboxAdded, KeyAdded, Send } from "../generated/schema"
+  Send as SendEvent,
+} from "../generated/Messaging/Messaging";
+import { InboxAdded, KeyAdded, Send } from "../generated/schema";
 
 export function handleInboxAdded(event: InboxAddedEvent): void {
   let entity = new InboxAdded(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.name = event.params.name
-  entity.description = event.params.description
-  entity.condition_nftContract = event.params.condition.nftContract
-  entity.condition_count = event.params.condition.count
-  entity.save()
+    event.transaction.from.toHex() + "-" + event.params.name.toString()
+  );
+  entity.owner = event.params.owner;
+  entity.name = event.params.name;
+  entity.description = event.params.description;
+  entity.condition_nftContract = event.params.condition.nftContract;
+  entity.condition_count = event.params.condition.count;
+  entity.save();
 }
 
 export function handleKeyAdded(event: KeyAddedEvent): void {
   let entity = new KeyAdded(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.sender = event.params.sender
-  entity.key = event.params.key
-  entity.save()
+  );
+  entity.sender = event.params.sender;
+  entity.key = event.params.key;
+  entity.save();
 }
 
 export function handleSend(event: SendEvent): void {
   let entity = new Send(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.sender = event.params.sender
-  entity.receiver = event.params.receiver
-  entity.content = event.params.content
-  entity.inboxName = event.params.inboxName
-  entity.encrypted = event.params.encrypted
-  entity.save()
+  );
+  entity.sender = event.params.sender;
+  entity.receiver = event.params.receiver;
+  entity.content = event.params.content;
+  entity.inboxName = event.params.inboxName;
+  entity.encrypted = event.params.encrypted;
+  entity.save();
 }
